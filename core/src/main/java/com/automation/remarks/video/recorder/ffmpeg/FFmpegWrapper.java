5ab9f39ab0cd6369e69119b6fe7ba5deb0a563cb
Aleksander Grzybowski <aleksander.grzybowski@capsilon.com>
2019-05-09 13:37:01 +0200

    Add more logging for better observability

--- core/src/main/java/com/automation/remarks/video/recorder/ffmpeg/FFmpegWrapper.java
+++ core/src/main/java/com/automation/remarks/video/recorder/ffmpeg/FFmpegWrapper.java
@@ -2,40 +2,40 @@ package com.automation.remarks.video.recorder.ffmpeg;
 
 import com.automation.remarks.video.DateUtils;
 import org.apache.commons.lang3.SystemUtils;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static com.automation.remarks.video.SystemUtils.*;
 import static com.automation.remarks.video.recorder.VideoRecorder.conf;
+import static java.util.Arrays.asList;
 
-/**
- * Created by sepi on 31.08.16.
- */
+@SuppressWarnings("ResultOfMethodCallIgnored")
 public class FFmpegWrapper {
 
-    private static final Logger log = org.apache.log4j.Logger.getLogger(FFMpegRecorder.class);
+    private static final Logger log = LoggerFactory.getLogger(FFMpegRecorder.class);
 
-    public static final String RECORDING_TOOL = "ffmpeg";
-    private static final String TEM_FILE_NAME = "temporary";
+    private static final String RECORDING_TOOL = "ffmpeg";
+    private static final String TEMP_FILE_NAME = "temporary";
     private static final String EXTENSION = ".mkv";
+    private static final String SEND_CTRL_C_TOOL_NAME = "SendSignalCtrlC.exe";
     private CompletableFuture<String> future;
     private File temporaryFile;
 
-    public void startFFmpeg(String... args) {
+    public void startFFmpeg(String... extraArgs) {
         File videoFolder = new File(conf().folder());
         if (!videoFolder.exists()) {
             videoFolder.mkdirs();
         }
 
         temporaryFile = getTemporaryFile();
-        final String[] commandsSequence = new String[]{
+        List<String> commandsSequence = asList(
                 FFmpegWrapper.RECORDING_TOOL,
                 "-y",
                 "-video_size", getScreenSize(),
@@ -45,36 +45,40 @@ public class FFmpegWrapper {
                 "-framerate", String.valueOf(conf().frameRate()),
                 "-pix_fmt", conf().ffmpegPixelFormat(),
                 temporaryFile.getAbsolutePath()
-        };
+        );
 
         List<String> command = new ArrayList<>();
-        command.addAll(Arrays.asList(commandsSequence));
-        command.addAll(Arrays.asList(args));
+        command.addAll(commandsSequence);
+        command.addAll(asList(extraArgs));
         this.future = CompletableFuture.supplyAsync(() -> runCommand(command));
     }
 
     public File stopFFmpegAndSave(String filename) {
-        String killLog = killFFmpeg();
-        log.info("Process kill output: " + killLog);
+        killFFmpeg();
 
         File destFile = getResultFile(filename);
-        this.future.whenCompleteAsync((out, errors) -> {
+        this.future.whenCompleteAsync((out, exception) -> {
             temporaryFile.renameTo(destFile);
-            log.debug("Recording output log: " + out + (errors != null ? "; ex: " + errors : ""));
-            log.info("Recording finished to: " + destFile.getAbsolutePath());
+            log.debug("Recording output log: " + out);
+            if (exception != null) {
+                log.error("Recording error: ", exception);
+            }
+            log.info("Recording output file: " + destFile.getAbsolutePath());
         });
         return destFile;
     }
 
-    private String killFFmpeg() {
-        final String SEND_CTRL_C_TOOL_NAME = "SendSignalCtrlC.exe";
-        return SystemUtils.IS_OS_WINDOWS ?
+    private void killFFmpeg() {
+        log.info("Killing ffmpeg process...");
+        String killLog = SystemUtils.IS_OS_WINDOWS ?
                 runCommand(SEND_CTRL_C_TOOL_NAME, getPidOf(RECORDING_TOOL)) :
                 runCommand("pkill", "-INT", RECORDING_TOOL);
+
+        log.info("Kill output: " + killLog);
     }
 
     public File getTemporaryFile() {
-        return getFile(TEM_FILE_NAME);
+        return getFile(TEMP_FILE_NAME);
     }
 
     public File getResultFile(String name) {
@@ -83,7 +87,7 @@ public class FFmpegWrapper {
 
     private File getFile(final String filename) {
         File movieFolder = new File(conf().folder());
-        final String name = filename + "_recording_" + DateUtils.formatDate(new Date(), "yyyy_dd_MM_HH_mm_ss");
+        String name = filename + "_recording_" + DateUtils.formatDate(new Date(), "yyyy_dd_MM_HH_mm_ss");
         return new File(movieFolder + File.separator + name + EXTENSION);
     }
 
